f79f6261745ce8b54ccf77ee6faa8f920f909c16
fatimato22 <fatimaelouardi187@gmail.com>
2025-12-03 20:45:37 +0100

    ajout ex10

--- ex10.js
+++ ex10.js
@@ -16,3 +16,14 @@ function sommeRecursive(n){
 let t=Number(prompt("saisir un nombre"));
  let r=factorielle(t);
  alert(r);
+ /* 1 - Quel code est le plus lisible ?
+   le code le plus lisible est la fonction itératif
+2 - Quel code est le plus performant ?
+ code est le plus performant est le code itératif car si on a un grand nombre la récursif consomme plus de mémoire 
+3 - La récursion peut-elle poser des problèmes ? Pourquoi ?
+ oui  dans le cas le nombre est trop grand 
+4 - Dans quels cas privilégier une boucle ? 
+  a- le nombre est grand 
+  b-optimiser de la mémoire 
+  c- pour une code plus lisible 
+  */
